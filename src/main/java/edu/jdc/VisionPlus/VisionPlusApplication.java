ecf8ba2ab44142380a641087432f0c4f562bea12
Sebas-Gomez13 <148719089+Sebas-Gomez13@users.noreply.github.com>
2024-09-11 14:28:01 -0500

    Subir Proyecto

---
+++ src/main/java/edu/jdc/VisionPlus/VisionPlusApplication.java
@@ -0,0 +1,13 @@
+package edu.jdc.VisionPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/jdc/VisionPlus/VisionPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.jdc.VisionPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
