0c25c3e35ce8fe79414e49be658cc7c0c120e917
JuliCorredorLopez <164068969+JuliCorredorLopez@users.noreply.github.com>
2024-09-18 21:26:46 -0500

    juli

--- src/main/java/edu/jdc/VisionPlus/daos/CitasDAO.java
+++ src/main/java/edu/jdc/VisionPlus/daos/CitasDAO.java
@@ -1,6 +1,48 @@
 package edu.jdc.VisionPlus.daos;
 
+import edu.jdc.VisionPlus.clases.Citas;
+import edu.jdc.VisionPlus.interfaces.Operacion;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class CitasDAO {
+
+@Service
+public class CitasDAO implements Operacion<Citas>{
+    
+    @Autowired
+    private CitaRepositorio repoCita;
+
+    @Override
+    public List<Citas> consultar(String orden) {
+        return (List<Citas>)repoCita.findAll();
+    }
+
+    @Override
+    public Boolean registrar(Citas objRegistrar) {
+        return repoCita.save(objRegistrar)!=null;
+    }
+
+    @Override
+    public Citas buscar(Integer llavePrimaria) {
+        return repoCita.findById(llavePrimaria).orElse(null);
+    }
+
+    @Override
+    public Boolean actualizar(Citas objActualizar) {
+        return repoCita.save(objActualizar)!=null;
+    }
+
+    @Override
+    public Boolean eliminar(Integer llavePrimaria) {
+         return true;
+//        return repoMarca.eliminarMarca(llavePrimaria);
+    }
+
+    @Override
+    public Integer cantidadRegistros() {
+//         return repoCita.cantidadMarcas();
+    return 0;
+    }
     
 }
--- src/main/java/edu/jdc/VisionPlus/interfaces/Operacion.java
+++ src/main/java/edu/jdc/VisionPlus/interfaces/Operacion.java
@@ -1,5 +1,13 @@
 package edu.jdc.VisionPlus.interfaces;
 
-public class Operacion {
-    
+import java.util.List;
+
+
+public interface Operacion<T> {
+     public List<T>consultar (String orden);
+    public Boolean registrar(T objRegistrar);
+    public T buscar (Integer llavePrimaria);
+    public Boolean actualizar (T objActualizar);
+    public Boolean eliminar(Integer llavePrimaria);
+    public Integer cantidadRegistros();
 }
