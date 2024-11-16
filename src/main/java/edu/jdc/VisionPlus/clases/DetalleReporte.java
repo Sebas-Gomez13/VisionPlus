4ca44293f3344caaf1ef7b1d2da915e7cbbcf94c
Sebas-Gomez13 <148719089+Sebas-Gomez13@users.noreply.github.com>
2024-11-16 14:16:45 -0500

    Update DetalleReporte.java

--- src/main/java/edu/jdc/VisionPlus/clases/DetalleReporte.java
+++ src/main/java/edu/jdc/VisionPlus/clases/DetalleReporte.java
@@ -1,5 +1,30 @@
 package edu.jdc.VisionPlus.clases;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name="detalle_reporte")
 public class DetalleReporte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_detalleReporte")
+    private Integer idDetalleReporte;
     
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "id_reporte", nullable = false)
+    private Reporte idReporte;
+    
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "id_producto", nullable = false)
+    private Producto idProducto;    
 }
